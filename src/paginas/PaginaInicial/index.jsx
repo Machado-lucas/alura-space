509fbce9bcc43562650354e1ff7a9848cd198187
Lucas Machado <borges.lucasmachado@gmail.com>
2023-06-09 16:15:43 -0300

    finalizando curso

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,33 @@
+import Cabecalho from "componentes/Cabecalho";
+import Galeria from "componentes/Galeria";
+import Menu from "componentes/Menu";
+import Populares from "componentes/Populares";
+
+
+import banner from "./banner.png"
+import styles from "./PaginaInicial.module.scss"
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="" />
+                    </div>
+                </section>
+
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
+            </main>
+
+        
+        </>
+
+    )
+}
